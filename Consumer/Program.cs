cc765d5c2feb4d1f0377632290bd0aa7f07eacb6
Maycon Silva <mayconsilva_cp@icloud.com>
2023-07-26 11:15:42 -0300

    Alterações, criado console leitura. Ainda alterando o projeto....

---
+++ Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using OrionMensageria.Dominio;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//Here we specify the Rabbit MQ Server. we use rabbitmq docker image and use it
+var message = (dynamic)null;
+var _factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using (var connection = _factory.CreateConnection())
+
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "bemvindo",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, eventArgs) =>
+    {
+        var body = eventArgs.Body.ToArray();
+        var contentString = Encoding.UTF8.GetString(body);
+        System.Diagnostics.Debug.WriteLine("Inicio a leitura");
+        
+        System.Diagnostics.Debug.WriteLine($"Primeira Opção {contentString}");
+        Console.WriteLine($"Segunda Opção {contentString}"); //este aqui
+
+        message = JsonConvert.DeserializeObject<Messages>(contentString);
+        System.Diagnostics.Debug.WriteLine($"Terceira Opção {message}");
+        Console.WriteLine($"Quarta Opção {message}");
+
+        // channel.BasicAck(eventArgs.DeliveryTag, false);
+    };
+   
+
+    channel.BasicConsume(queue: "bemvindo",
+                         autoAck: true,
+                         consumer: consumer);
+}
+Console.ReadKey();
--- OrionMensageria/Controllers/MessengerController.cs
+++ OrionMensageria/Controllers/MessengerController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using OrionMensageria.Dominio;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
 using System.Text;
+using System.Threading.Channels;
 
 namespace OrionMensageria.Controllers
 {
@@ -32,9 +37,10 @@ namespace OrionMensageria.Controllers
                                      autoDelete: false,
                                      arguments: null);
 
-                string postMensagem = objMsg.mensagem;
+                // string postMensagem = objMsg.mensagem;
+                var json = JsonConvert.SerializeObject(objMsg);
 
-                var body = Encoding.UTF8.GetBytes(postMensagem);
+                var body = Encoding.UTF8.GetBytes(json);
 
                 channel.BasicPublish(exchange: "",
                                      routingKey: "bemvindo",
@@ -43,6 +49,46 @@ namespace OrionMensageria.Controllers
             }
 
             return Ok();
+        }
+
+        [HttpGet("readMessage")]
+        public async Task<IActionResult> readMessage()
+        {
+            var message = (dynamic)null;
+
+            using (var connection = _factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "bemvindo",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, eventArgs) =>
+                {
+                    var body = eventArgs.Body.ToArray();
+                    var contentString = Encoding.UTF8.GetString(body);
+                    System.Diagnostics.Debug.WriteLine("Inicio a leitura");
+                    System.Diagnostics.Debug.WriteLine($"1 Tentativa leitura {contentString}");
+
+                    message = JsonConvert.DeserializeObject<Messages>(contentString);
+                    System.Diagnostics.Debug.WriteLine($"2 Tentativa leitura {message}");
+
+                    // channel.BasicAck(eventArgs.DeliveryTag, false);
+                };
+                System.Diagnostics.Debug.WriteLine("Inicio a leitura");
+                
+                System.Diagnostics.Debug.WriteLine($"2 Tentativa leitura {message}");
+
+                channel.BasicConsume(queue: "bemvindo",
+                                     autoAck: true,
+                                     consumer: consumer);
+            }
+            return Ok(message);
         }
     }
 }
