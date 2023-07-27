f5b12687b858302e474aa125007fdabff63cc65f
Maycon Silva <mayconsilva_cp@icloud.com>
2023-07-27 16:09:54 -0300

    Melhoria, incluido um list para ler e armazenar mais de um registro.

--- OrionMensageria/Controllers/MessengerController.cs
+++ OrionMensageria/Controllers/MessengerController.cs
@@ -54,11 +54,12 @@ namespace OrionMensageria.Controllers
         [HttpGet("readMessage")]
         public async Task<IActionResult> readMessage()
         {
-            var notification = (dynamic)null;
-            var _factory = new ConnectionFactory()
-            {
-                HostName = "localhost"
-            };
+            var listNotification = new List<Notification_Mensageria>();
+
+            //var _factory = new ConnectionFactory()
+            //{
+            //    HostName = "localhost"
+            //};
 
             using (var connection = _factory.CreateConnection())
 
@@ -76,39 +77,27 @@ namespace OrionMensageria.Controllers
                 {
                     var body = eventArgs.Body.ToArray();
                     var contentString = Encoding.UTF8.GetString(body);
-                    //System.Diagnostics.Debug.WriteLine("Inicio a leitura");
-
-                    //System.Diagnostics.Debug.WriteLine($"Primeira Opção {contentString}");
-                    Console.WriteLine($"Segunda Opção {contentString}"); //este aqui
 
                     var message = JsonConvert.DeserializeObject<Notification_Mensageria>(contentString);
-                    notification = new Notification_Mensageria()
+                    var notification = new Notification_Mensageria()
                     {
                         tipo = message.tipo,
                         assunto = message.assunto,
                         cliente = message.cliente,
                         mensagem = message.mensagem
                     };
-
-                    Console.WriteLine();
-                    // message = JsonConvert.DeserializeObject<Messages>(contentString);
-                    //System.Diagnostics.Debug.WriteLine($"Terceira Opção {message}");
-                    // Console.WriteLine($"Quarta Opção {message}");
-
-                    // channel.BasicAck(eventArgs.DeliveryTag, false);
+                    listNotification.Add(notification);
+                    
+                    System.Diagnostics.Debug.WriteLine("Passou e leu " + listNotification.Count());
+                    //channel.BasicAck(eventArgs.DeliveryTag, false);
                 };
                
-
                 channel.BasicConsume(queue: "bemvindo",
                                      autoAck: true,
                                      consumer: consumer);
-
-                
-                Console.WriteLine();
-                
             }
-            Console.WriteLine();
-            return Ok(notification);
+            
+            return Ok(listNotification);
         }
 
     }
